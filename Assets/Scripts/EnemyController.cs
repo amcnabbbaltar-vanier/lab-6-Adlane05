849c7a49f1c8e2e42de9c6d86656f97cd6acfef1
Adlane05 <143427032+Adlane05@users.noreply.github.com>
2025-10-06 18:01:39 -0400

    finished

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,18 +2,36 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private float health = 0;
     private Animator animator;
+    private EnemyHealth enemyHealth;
     private ParticleSystem system;
+    private AudioSource audioSource;
     void Start()
     {
         animator = GetComponent<Animator>();
         system = GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
+        enemyHealth = GetComponent<EnemyHealth>();
+
     }
 
     public void GotHit()
     {
-        animator.ResetTrigger("GotHit");
-        animator.SetTrigger("GotHit");
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(10);
+            animator.SetTrigger("GotHit");
+            system.Play();
+            audioSource.Play();
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.AddScore(1); // Add 1 point per hit
+            }
+
+
+        }
 
     }
+       
 }
